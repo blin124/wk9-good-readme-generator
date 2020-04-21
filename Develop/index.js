cfdeb3e5c6312f2867dfa6062235fa2db6d186b9
Cynthia Zhou <lbx01072004@gmail.com>
2020-04-21 17:44:52 +1000

    includes questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,103 @@
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-];
+function template({imageHref, imageSrc, imageTitle, imageAlt, name, title, description, contents, installation, usage, license, contributors, testConduct, testInclusion, questions}) {  
+
+    return 
+    `
+
+    `
+}
+
+inquirer
+    .prompt([{
+        type: "confirm",
+        message: "Please include image", 
+        name: "imageConfirm"
+    },
+    {
+        type: "input",
+        message: "What is the website for the image?", 
+        name: "imageHref"
+    },
+    {
+        type: "input",
+        message: "What is the name of your image source?", 
+        name: "imageSrc"
+    },
+    {
+        type: "input",
+        message: "Please enter a title for this image.", 
+        name: "imageTitle"
+    },
+    {
+        type: "input",
+        message: "Please enter an alt for this image.", 
+        name: "imageAlt"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?", 
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is your description for the project?",
+        name: "description",
+    },
+    {   
+        type: "list",
+        message: "Here is your contents list",
+        // message: "Select the contents of your Table of Contents",
+        name: "contents",
+        choices: [
+        "Project Title",
+        "Description",
+        "Installation",
+        "Usage",
+        "License",
+        "Contributing",
+        "Tests",
+        "Questions"
+        ],
+    }, 
+      
+    {   type: "input",
+        message: "What would be the installation operator?",
+        name: "installation",
+    },   
+    {   type: "input",
+        message: "What is the main purpose for usage of this project?",
+        name: "usage",
+    },   
+    {   type: "input",
+        message: "Please input an appropriate license for this project",
+        name: "license",
+    },   
+    {   type: "input",
+        message: "Please list contributors of this project.",
+        name: "contributors",
+    },   
+    {   type: "input",
+        message: "Have tests been conducted?",
+        name: "testConduct",
+    },   
+    {   type: "input",
+        message: "Have these tests been included in project contents?",
+        name: "testInclusion",
+    },   
+    {   type: "input",
+        message: "Please include any further questions.",
+        default: "Unknown issue? There is always google there to help you.",
+        name: "questions",
+    },   
+    ])
+
 
 function writeToFile(fileName, data) {
 }
@@ -10,3 +107,4 @@ function init() {
 }
 
 init();
+
--- README 2.md
+++ README 2.md
@@ -37,7 +37,12 @@ Refer to the [Good README guide](../../01-HTML-Git-CSS/04-Supplemental/Good-READ
 
 ## Business Context
 
-When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
+When creating an open source project on GitHub, it is important to have a quality README with information about the app--
+-what is the app for, 
+-how to use the app, 
+-how to install it, 
+-how to report issues, and 
+-how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
 
 ## Minimum Requirements
 
