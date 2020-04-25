13c2ab3f4464ad0183c5becea4bb253e31fa9a87
Cynthia Zhou <lbx01072004@gmail.com>
2020-04-25 14:40:34 +1000

    add files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,32 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const axios = require("axios");
 
-function template({imageHref, imageSrc, imageTitle, imageAlt, name, title, description, contents, installation, usage, license, contributors, testConduct, testInclusion, questions}) {  
+function template({name, title, subTitle, description, contents, installation, usage, license, contributors, testConduct, testInclusion, questions}) {  
 
-    return 
-    `
+    return `
+    https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
+
+    
+
+
 
     `
 }
+
+function getUser(username) {
+    return axios
+        .get(
+        `https://api.github.com/users/${username}`
+        )
+        .catch(err => {
+        console.log(`User not found`);
+        process.exit(1);
+    });
+}
+
+
+
 
 inquirer
     .prompt([{
@@ -17,37 +36,25 @@ inquirer
     },
     {
         type: "input",
-        message: "What is the website for the image?", 
-        name: "imageHref"
-    },
-    {
-        type: "input",
-        message: "What is the name of your image source?", 
-        name: "imageSrc"
-    },
-    {
-        type: "input",
-        message: "Please enter a title for this image.", 
-        name: "imageTitle"
-    },
-    {
-        type: "input",
-        message: "Please enter an alt for this image.", 
-        name: "imageAlt"
-    },
-    {
-        type: "input",
         message: "What is your Github username?", 
         name: "name"
     },
     {
         type: "input",
         message: "What is your project title?",
+        default: "Project title goes here",
         name: "title",
     },
     {
         type: "input",
+        message: "Please type in a subtitle for project?",
+        default: "Subtitle",
+        name: "subTitle",
+    },
+    {
+        type: "input",
         message: "What is your description for the project?",
+        default: "A description is ideally one sentence. Also, include any terms or tags that can be searched.",
         name: "description",
     },
     {   
@@ -66,7 +73,6 @@ inquirer
         "Questions"
         ],
     }, 
-      
     {   type: "input",
         message: "What would be the installation operator?",
         name: "installation",
@@ -95,16 +101,35 @@ inquirer
         message: "Please include any further questions.",
         default: "Unknown issue? There is always google there to help you.",
         name: "questions",
-    },   
-    ])
+    }
+]).then((inquirerResponses) => {
+    console.log(inquirerResponses)
+    getUser(inquirerResponses.name)
+      .then((githubResponse) => {
+        console.log(githubResponse.data.avatar_url);
+    })
+})
+
+
+
+function askUser() {
+    // inquirer
+}
+
+function generateReadme(userResponse) {
+    // generate the readme and return a string
+}
 
 
 function writeToFile(fileName, data) {
+    // use package fs 
 }
 
-function init() {
-
+function firstThing() {
+    // const userResponse = askUser();
+    // const generatedReadme = generateReadme(userResponse);
+    // writeToFile('README.md', generatedReadme);
 }
 
-init();
+firstThing();
 
